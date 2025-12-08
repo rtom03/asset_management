4cfc445f8fff2bf2d0086985e2d66aed8795e390
Tom <zuckm709@gmail.com>
2025-12-08 15:44:23 +0100

    implement restructure of user model and append two more model

--- backend/controller/locationController.js
+++ backend/controller/locationController.js
@@ -3,19 +3,38 @@ import { prisma } from "../utils/db";
 export const createLocation = async (req, res) => {
   const { name, people, address, city, state } = req.body;
   try {
-    const data = await prisma.location.create({
-      data: {
-        name,
-        people,
-        address,
-        city,
-        state,
-      },
+    const locationExist = await prisma.location.findUnique({
+      where: { name: String(name) },
     });
 
-    res
-      .status(201)
-      .json({ message: "Location created successfully", data: res });
+    if (!locationExist) {
+      const data = await prisma.location.create({
+        data: {
+          name,
+          people,
+          address,
+          city,
+          state,
+        },
+      });
+
+      return res.status(201).json({
+        message: "Location created successfully",
+        data: {
+          id: data.id,
+          name: data.name,
+          people: data.people,
+          address: data.address,
+          city: data.city,
+          state: data.state,
+        },
+      });
+    }
+    return res
+      .status(400)
+      .json({
+        message: `A location with the name ${name} already exist....try again with a unique value`,
+      });
   } catch (error) {
     console.log(error);
   }
